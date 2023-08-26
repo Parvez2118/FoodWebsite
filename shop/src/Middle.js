fff699dfcc192d3d2a88b1a0007b950cd5f9dab9
Parvez2118 <140383618+Parvez2118@users.noreply.github.com>
2023-08-26 17:44:33 +0530

    adding updated fies

--- shop/src/Middle.js
+++ shop/src/Middle.js
@@ -2,6 +2,45 @@
 function Middle(){
     return(
         <>
+
+<div id="carouselExampleCaptions" class="carousel slide carousel-fade" style={{ height:600+"px"}}>
+  <div class="carousel-indicators">
+    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
+    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
+  </div>
+  <div class="carousel-inner" style={{ height:600+"px"}}>
+    <div class="carousel-item active"style={{ height:600+"px"}} >
+      <img src="https://media.istockphoto.com/id/1054228718/photo/indian-sweets-in-a-plate-includes-gulab-jamun-rasgulla-kaju-katli-morichoor-bundi-laddu.jpg?s=612x612&w=0&k=20&c=hYWCXLaldKvhxdBa83M0RnUij7BCmhf-ywWdvyIXR40=" class="d-block w-100 " alt="..." style={{ height:600+"px"}}/>
+      <div class="carousel-caption d-none d-md-block">
+        <h5>First slide label</h5>
+        <p>Some representative placeholder content for the first slide.</p>
+      </div>
+    </div>
+    <div class="carousel-item" style={{ height:600+"px"}}>
+      <img src="https://th.bing.com/th/id/OIP.gH0X4YaD9kIFm791CLR2HQHaEK?pid=ImgDet&rs=1" class="d-block w-100 " alt="..." style={{ height:600+"px"}}/>
+      <div class="carousel-caption d-none d-md-block">
+        <h5>Second slide label</h5>
+        <p>Some representative placeholder content for the second slide.</p>
+      </div>
+    </div>
+    <div class="carousel-item" style={{ height:600+"px"}}>
+      <img src="https://th.bing.com/th/id/OIP.aLgSsbuKmc5yA_jveGZaiAHaE7?pid=ImgDet&rs=1" class="d-block w-100 " alt="..." style={{ height:600+"px"}}/>
+      <div class="carousel-caption d-none d-md-block">
+        <h5>Third slide label</h5>
+        <p>Some representative placeholder content for the third slide.</p>
+      </div>
+    </div>
+  </div>
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
+</div>
         
 <div style={{justifyContent:"center", textAlign:"center",border:1+"px solid yellow",padding:10+"px",fontSize:30+"px",marginTop:20+"px"}}>
 North Indian Sweets
--- shop/src/Navbar.js
+++ shop/src/Navbar.js
@@ -2,12 +2,12 @@
 function Navbar() {
   return (
     <>
-    <nav class="navbar " style={{backgroundColor:"#FFFD8C"}} >
+    <nav class="navbar " style={{backgroundColor:"#FFF9B0"}} >
   <div class="container-fluid">
-    <a class="navbar-brand">Indian Sweet Shop</a>
+    <a class="navbar-brand"   style={{fontFamily:"'Dancing Script', cursive", fontSize:40+"px",color:"#42032C"}}>RajKishor Sweet Shop</a>
     <form class="d-flex" role="search">
       <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
-      <button class="btn btn-outline-success" type="submit">Search</button>
+      <button class="btn " type="submit">Search</button>
     </form>
     
     <a class="navbar-brand">Login | Register</a>
@@ -17,20 +17,23 @@ function Navbar() {
   </div>
 </nav>
 
-<nav class="navbar navbar-expand-lg " style={{backgroundColor:"#F0B86E"}}>
+<nav class="navbar navbar-expand-lg " style={{backgroundColor:"#E38B29",color:"red", fontFamily: "Microsoft Sans Serif", fontSize:"medium", fontWeight: "light"}}>
   <div class="container-fluid">
    
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
+
+    {/* style={{color:"#42032C",marginRight:20+"px"}} */}
     <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-      <div class="navbar-nav">
-        <a class="nav-link" href="#" >Home</a>
-        <a class="nav-link" href="#" >Sweets</a>
-        <a class="nav-link" href="#">Snacks</a>
-        <a class="nav-link" href="#" >Staples</a>
-        <a class="nav-link" href="#" >Shop By State</a>
-        <a class="nav-link" href="#">Must Try</a>
+      <div class="navbar-nav"  >
+        <a class="nav-link nav1 nav2 " href="#" >Home</a>
+        <a class="nav-link nav1 nav2" href="#" >Sweets</a> 
+        <a class="nav-link nav1 nav2" href="#" >Sweets</a> 
+        <a class="nav-link nav1 nav2" href="#">Snacks</a>
+        <a class="nav-link nav1 nav2" href="#" >Staples</a>
+        <a class="nav-link nav1 nav2" href="#">Shop By State</a>
+        <a class="nav-link nav1 nav2" href="#">Must Try</a>
         {/* <a class="nav-link" href="#" style={{marginRight:105+"px"}}>Login</a>
         <a class="nav-link" href="#" style={{marginRight:105+"px"}}>Login | Register</a> */}
       </div>
